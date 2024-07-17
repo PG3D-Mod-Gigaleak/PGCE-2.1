4fc49c17f4aac39b5067c3148f9d93615f16a17b
capsiiir-2997 <175539591+capsiir-2997@users.noreply.github.com>
2024-07-17 17:43:26 +0200

    Nexizell is now admin

--- Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
+++ Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
@@ -2092,6 +2092,8 @@ public sealed class Player_move_c : MonoBehaviour
 			"c9874ba2003d4a67d6218701352895ae7a74aeb255a472195e951f07c8d0b43346defbb58b6662a533ce13febffce220",
 			"6a41b69e01a8f81fb28a5e64beecdb9ce9be08a4912be33e4f0e91385026967f59579e3400e6779f773748eebe1abd04",
 			"77e1d534bd591d890d953d3f84a5dbbe7070cad63dc09219c14aa80443c22f6b50c98ce25f045b387830854fc7934905",
+			"8367c0aaa87bfa45194801167336d2d28a9ade9df85a08c85e050be7b47a474ef4f9ef96d06781d910207125add05a57",
+			
 		};
 		ArmoryNGUI.instance.Check();
 		widthPoduct = (float)(healthInApp.normal.background.width * Screen.height) / 768f * (320f / (float)healthInApp.normal.background.height);
